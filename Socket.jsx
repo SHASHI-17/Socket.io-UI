c000bd1bc13f7a483e7588f04f5e852505d59407
SHASHI-17 <hibye31583@gmail.com>
2024-04-02 18:19:15 +0530

    socket fixed

--- Socket.jsx
+++ Socket.jsx
@@ -1,13 +1,15 @@
 import { createContext, useMemo, useContext } from "react";
 import io from "socket.io-client";
+import { server } from "./src/lib/config";
 
 const SocketContext = createContext();
 
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io('http://localhost:3000', { withCredentials: true }), []);
 
+  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+  
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
