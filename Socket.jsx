f7bd1cf6ff70fe20b4448b13f9864d056ae9e423
SHASHI-17 <hibye31583@gmail.com>
2024-04-02 18:29:41 +0530

    socket

--- Socket.jsx
+++ Socket.jsx
@@ -7,8 +7,8 @@ const SocketContext = createContext();
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-
-  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+console.log(server);
+  const socket = useMemo(() => io('https://socket-io-server-cxw8.onrender.com/', { withCredentials: true }), []);
   
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
